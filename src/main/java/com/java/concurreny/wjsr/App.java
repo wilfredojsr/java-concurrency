a8055c28be92e9ec391c9c711d2fa0fd08769dbe
WISUAREZ-AR\wisuarez <wilfredo.suarez@oracle.com>
2019-12-20 14:25:20 -0300

    Initial commit

---
+++ src/main/java/com/java/concurreny/wjsr/App.java
@@ -0,0 +1,13 @@
+package com.java.concurreny.wjsr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
