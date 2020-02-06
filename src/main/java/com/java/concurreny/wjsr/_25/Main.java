d7e9cff19b77f34e8578307e975e3731d3bcd6eb
WISUAREZ-AR\wisuarez <wilfredo.suarez@oracle.com>
2020-02-06 18:36:18 -0300

    _25 Executing tasks in an executor that returns a result

---
+++ src/main/java/com/java/concurreny/wjsr/_25/Main.java
@@ -0,0 +1,67 @@
+package com.java.concurreny.wjsr._25;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors
+                .newFixedThreadPool(2);
+
+        List<Future<Integer>> resultList = new ArrayList<>();
+        Random random = new Random();
+
+        IntStream.range(0, 10).forEach(i -> {
+
+            Integer number = random.nextInt(10);
+            FactorialCalculator calculator = new FactorialCalculator(number);
+
+            Future<Integer> result = executor.submit(calculator);
+            resultList.add(result);
+
+        });
+
+        do {
+
+            System.out.printf("Main: Number of Completed Tasks: %d\n",
+                    executor.getCompletedTaskCount());
+
+            IntStream.range(0, 10).forEach(i -> {
+                Future<Integer> result = resultList.get(i);
+                System.out.printf("Main: Task %d: %s\n", i, result.isDone());
+            });
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } while (executor.getCompletedTaskCount() < resultList.size());
+
+        System.out.printf("Main: Results\n");
+
+        IntStream.range(0, resultList.size()).forEach(i -> {
+
+            Future<Integer> result = resultList.get(i);
+            Integer number = null;
+
+            try {
+                number = result.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            System.out.printf("Main: Task %d: %d\n", i, number);
+        });
+
+        executor.shutdown();
+    }
+}
