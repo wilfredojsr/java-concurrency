1189522f22cceb311f46d1df03094915a7cbdfa2
WISUAREZ-AR\wisuarez <wilfredo.suarez@oracle.com>
2020-02-12 11:57:36 -0300

    _32 Separating the launching of tasks and the processing of their results in an executor

---
+++ src/main/java/com/java/concurreny/wjsr/_32/ReportProcessor.java
@@ -0,0 +1,41 @@
+package com.java.concurreny.wjsr._32;
+
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class ReportProcessor implements Runnable {
+
+    private final CompletionService<String> service;
+    private volatile boolean end;
+
+
+    public ReportProcessor(CompletionService<String> service) {
+        this.service = service;
+        end = false;
+    }
+
+    @Override
+    public void run() {
+        while (!end) {
+            try {
+                Future<String> result = service.poll(20, TimeUnit.SECONDS);
+
+                if (result != null) {
+                    String report = result.get();
+                    System.out.printf("ReportReceiver: Report Received: %s\n",
+                            report);
+                }
+
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.printf("ReportSender: End\n");
+    }
+
+    public void stopProcessing() {
+        this.end = true;
+    }
+}
